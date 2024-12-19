e784ea86fe9b8483c877d32c81ba4163ecbee38e
wintrysec <wintrysec@gmail.com>
2024-12-19 16:42:46 +0800

    push

---
+++ assets/Linux更换更新源.html.83dcdbf9.js
@@ -0,0 +1,26 @@
+import{_ as s}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as e,c as n,e as a}from"./app.04d5a320.js";const r={},t=a(`<h2 id="centos-7" tabindex="-1"><a class="header-anchor" href="#centos-7" aria-hidden="true">#</a> Centos 7</h2><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code><span class="token function">curl</span> <span class="token parameter variable">-o</span> /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-7.repo
+<span class="token function">curl</span> <span class="token parameter variable">-o</span> /etc/yum.repos.d/epel.repo http://mirrors.cloud.tencent.com/repo/epel-7.repo
+yum clean all<span class="token punctuation">;</span>yum makecache
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="centos-8" tabindex="-1"><a class="header-anchor" href="#centos-8" aria-hidden="true">#</a> Centos 8</h2><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code><span class="token comment">#公网用户</span>
+<span class="token function">mv</span> /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
+<span class="token function">wget</span> <span class="token parameter variable">-O</span> /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-vault-8.5.2111.repo
+yum clean all <span class="token operator">&amp;&amp;</span> yum makecache
+
+<span class="token comment">#阿里云ECS用户</span>
+<span class="token function">mv</span> /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
+<span class="token function">wget</span> <span class="token parameter variable">-O</span> /etc/yum.repos.d/CentOS-Base.repo http://mirrors.cloud.aliyuncs.com/repo/Centos-vault-8.5.2111.repo
+<span class="token function">sed</span> <span class="token parameter variable">-i</span> <span class="token string">&#39;s/mirrors.cloud.aliyuncs.com/url_tmp/g&#39;</span> /etc/yum.repos.d/CentOS-Base.repo <span class="token operator">&amp;&amp;</span> <span class="token function">sed</span> <span class="token parameter variable">-i</span> <span class="token string">&#39;s/mirrors.aliyun.com/mirrors.cloud.aliyuncs.com/g&#39;</span> /etc/yum.repos.d/CentOS-Base.repo <span class="token operator">&amp;&amp;</span> <span class="token function">sed</span> <span class="token parameter variable">-i</span> <span class="token string">&#39;s/url_tmp/mirrors.aliyun.com/g&#39;</span> /etc/yum.repos.d/CentOS-Base.repo
+yum clean all <span class="token operator">&amp;&amp;</span> yum makecache
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="ubuntu" tabindex="-1"><a class="header-anchor" href="#ubuntu" aria-hidden="true">#</a> Ubuntu</h2><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code><span class="token function">cat</span> <span class="token operator">&gt;</span> /etc/apt/sources.list <span class="token operator">&lt;&lt;</span><span class="token string">EOF
+deb https://mirrors.ustc.edu.cn/ubuntu/ focal main restricted universe multiverse
+deb https://mirrors.ustc.edu.cn/ubuntu/ focal-security main restricted universe multiverse
+deb https://mirrors.ustc.edu.cn/ubuntu/ focal-updates main restricted universe multiverse
+deb https://mirrors.ustc.edu.cn/ubuntu/ focal-backports main restricted universe multiverse
+EOF</span>
+<span class="token function">sudo</span> <span class="token function">apt-get</span> update
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="kali-linux" tabindex="-1"><a class="header-anchor" href="#kali-linux" aria-hidden="true">#</a> Kali Linux</h2><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code><span class="token function">cat</span> <span class="token operator">&gt;</span> /etc/apt/sources.list <span class="token operator">&lt;&lt;</span><span class="token string">EOF
+deb https://mirrors.ustc.edu.cn/kali kali-rolling main non-free contrib
+deb-src https://mirrors.ustc.edu.cn/kali kali-rolling main non-free contrib
+EOF</span>
+<span class="token function">sudo</span> <span class="token function">apt-get</span> update
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,8),i=[t];function c(l,o){return e(),n("div",null,i)}const d=s(r,[["render",c],["__file","Linux更换更新源.html.vue"]]);export{d as default};
---
+++ assets/开机启动项.html.7e96e3e0.js
@@ -0,0 +1,59 @@
+import{_ as n}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as s,c as i,e}from"./app.04d5a320.js";const a={},d=e(`<h2 id="一、systemd启动" tabindex="-1"><a class="header-anchor" href="#一、systemd启动" aria-hidden="true">#</a> 一、systemd启动</h2><div class="custom-container info"><p class="custom-container-title">相关信息</p><p>systemd提供更优秀的框架以表示系统服务间的依赖关系</p><p>并依此实现系统初始化时服务的并行启动，同时达到降低Shell的系统开销的效果</p><p>(1) 采用Socket激活式与总线激活式服务，以提高相互依赖的各服务的并行运行性能；</p><p>(2) 用Cgroups代替PID来追踪进程，因此即使是两次fork之后生成的守护进程也不会脱离systemd的控制</p></div><h3 id="示例-mongodb数据库开机自启动" tabindex="-1"><a class="header-anchor" href="#示例-mongodb数据库开机自启动" aria-hidden="true">#</a> 示例-mongodb数据库开机自启动</h3><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code><span class="token function">cat</span> <span class="token operator">&gt;</span> /usr/lib/systemd/system/mongod.service <span class="token operator">&lt;&lt;</span><span class="token string">EOF
+
+[Unit]
+Description=MongoDB Database Server
+Documentation=https://docs.mongodb.org/manual
+After=network.target
+
+[Service]
+User=mongod
+Group=mongod
+Environment=&quot;OPTIONS=-f /etc/mongod.conf&quot;
+EnvironmentFile=-/etc/sysconfig/mongod
+ExecStart=/usr/bin/mongod <span class="token variable">$OPTIONS</span>
+ExecStartPre=/usr/bin/mkdir -p /var/run/mongodb
+ExecStartPre=/usr/bin/chown mongod:mongod /var/run/mongodb
+ExecStartPre=/usr/bin/chmod 0755 /var/run/mongodb
+PermissionsStartOnly=true
+PIDFile=/var/run/mongodb/mongod.pid
+Type=forking
+
+# file size
+LimitFSIZE=infinity
+# cpu time
+LimitCPU=infinity
+# virtual memory size
+LimitAS=infinity
+# open files
+LimitNOFILE=64000
+# processes/threads
+LimitNPROC=64000
+# locked memory
+LimitMEMLOCK=infinity
+# total threads (user+kernel)
+TasksMax=infinity
+TasksAccounting=false
+
+[Install]
+WantedBy=multi-user.target
+EOF</span>
+
+<span class="token comment">#设置mongod开机自启</span>
+systemctl <span class="token builtin class-name">enable</span> mongod
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="二、自定义脚本启动" tabindex="-1"><a class="header-anchor" href="#二、自定义脚本启动" aria-hidden="true">#</a> 二、自定义脚本启动</h2><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code><span class="token function">cat</span> <span class="token operator">&gt;</span> /etc/init.d/startapi.sh <span class="token operator">&lt;&lt;</span><span class="token string">EOF
+#!/bin/sh
+#chkconfig: 2345 80 90
+#description:auto_run
+nohup /root/api &amp;
+EOF</span>
+
+<span class="token function">chmod</span> +x /etc/init.d/startapi.sh
+<span class="token function">chkconfig</span> <span class="token parameter variable">--add</span> startapi.sh
+<span class="token function">chkconfig</span> startapi.sh on
+
+<span class="token comment">#移除此启动项</span>
+<span class="token function">chkconfig</span> startapi.sh off
+<span class="token function">chkconfig</span> <span class="token parameter variable">--del</span> startapi.sh
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="三、使用定时任务" tabindex="-1"><a class="header-anchor" href="#三、使用定时任务" aria-hidden="true">#</a> 三、使用定时任务</h2><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code><span class="token function">chmod</span> +x /home/startapi.sh
+<span class="token builtin class-name">echo</span> <span class="token string">&quot;* * * * * root /home/startapi.sh&quot;</span> <span class="token operator">&gt;&gt;</span> /etc/crontab
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div>`,8),l=[d];function r(t,c){return s(),i("div",null,l)}const m=n(a,[["render",r],["__file","开机启动项.html.vue"]]);export{m as default};
